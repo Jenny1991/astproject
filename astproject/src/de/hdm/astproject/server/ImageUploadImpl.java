0b2a0f49f7b02749fce24c31970b14237246af85
Jenny <Jenny>
2015-01-08 15:04:20 +0100

    UploadImpl

--- astproject/src/de/hdm/astproject/server/ImageUploadImpl.java
+++ astproject/src/de/hdm/astproject/server/ImageUploadImpl.java
@@ -2,6 +2,8 @@ package de.hdm.astproject.server;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -16,6 +18,13 @@ import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.IOUtils;
+
+import com.google.appengine.api.urlfetch.FetchOptions;
+import com.google.appengine.api.urlfetch.HTTPMethod;
+import com.google.appengine.api.urlfetch.HTTPRequest;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 
 /**
  * The server-side implementation of the RPC service.
@@ -57,5 +66,29 @@ public class ImageUploadImpl extends HttpServlet {
 		}
 
 	}
+	
+	public String fetch(String postdata) throws IOException {
+
+	    String surl = "http://130.211.101.46";
+
+	    URLFetchService urlFetchService = URLFetchServiceFactory.getURLFetchService();
+
+	    URL url = new URL(surl);
+
+	    FetchOptions fetchOptions = FetchOptions.Builder.withDefaults();
+	    fetchOptions.doNotValidateCertificate();
+	    fetchOptions.setDeadline(60D);
+
+	    HTTPRequest request = new HTTPRequest(url, HTTPMethod.POST, fetchOptions);
+	    request.setPayload(postdata.getBytes());
+	    
+	    HTTPResponse httpResponse = urlFetchService.fetch(request);
+	    String response = "";
+	    if (httpResponse.getResponseCode() == HttpURLConnection.HTTP_OK) {
+	      response = new String(httpResponse.getContent());
+	    } 
+
+	    return response;
+	  }
 	
 }
